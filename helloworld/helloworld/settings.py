121f55d7ee65050d54de5795cbe68832d8efa0ca
Chandra Reddy <ch807460@CMC02WK1HQHTDF.local>
2024-02-28 10:42:11 -0500

    fix django issues

--- helloworld/helloworld/settings.py
+++ helloworld/helloworld/settings.py
@@ -13,14 +13,14 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@cq2aolk@*t#e%q5xf0oa_7*2=7$g@1qkq3(enqz%k586ihudr'
+SECRET_KEY = 'django-insecure-_i_xqo-9_5%du03@lxqtkk&11r*21=y$tl()u#cd@eo55dh3-@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'helloworld'
 ]
 
 MIDDLEWARE = [
