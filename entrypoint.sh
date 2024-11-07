d638bdcaab4b970a7f0470952e9c8f081075900e
ucfchandra <150701106+ucfchandra@users.noreply.github.com>
2024-11-07 15:15:56 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cd /app
+cd /app/mysite
 python manage.py migrate
 exec gunicorn --workers=2 --threads=4 --worker-class=gthread --bind '[::]:80' --worker-tmp-dir /dev/shm wsgi:application
 
